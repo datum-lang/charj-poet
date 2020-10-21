86b64bf9565d703db3e4532f27bfaacb1b1ce76c
Phodal Huang <h@phodal.com>
2020-10-21 12:41:50 +0800

    feat: init method spec

--- src/poet/method_spec.rs
+++ src/poet/method_spec.rs
@@ -1,5 +1,19 @@
+use crate::poet::type_name::TypeName;
+use crate::poet::modifier::Modifier;
+use crate::poet::parameter_spec::ParameterSpec;
+use crate::poet::annotation_spec::AnnotationSpec;
+use crate::poet::code_block::CodeBlock;
+
 /** A generated constructor or method declaration. */
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct MethodSpec {
-
+    pub name: String,
+    pub modifiers: Vec<Modifier>,
+    pub receiver_type: TypeName,
+    pub return_type: TypeName,
+    pub parameters: Vec<ParameterSpec>,
+    pub annotations: Vec<AnnotationSpec>,
+    pub code: CodeBlock,
 }
+
+impl MethodSpec {}
