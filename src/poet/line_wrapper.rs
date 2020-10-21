6330c927cd54990224f701281c3fe628f32de504
Phodal Huang <h@phodal.com>
2020-10-21 20:32:52 +0800

    feat: init line wrapper

--- src/poet/line_wrapper.rs
+++ src/poet/line_wrapper.rs
@@ -3,4 +3,28 @@
 /// or soft-wrapping spaces using {@link #wrappingSpace}.
 ///
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct LineWrapper {}
+pub struct LineWrapper {
+    pub out: String,
+    pub indent: String,
+    pub statement_line: i32,
+    pub segments: Vec<String>,
+    pub indent_level: i32,
+    pub line_prefix: String,
+}
+
+impl LineWrapper {
+    pub fn new(out: String, indent: String, statement_line: i32) -> Self {
+        LineWrapper {
+            out,
+            indent,
+            statement_line,
+            segments: vec!["".to_string()],
+            indent_level: -1,
+            line_prefix: "".to_string(),
+        }
+    }
+
+    pub fn has_pending_segments(&self) -> bool {
+        self.segments.len() != 1 || !self.segments[0].is_empty()
+    }
+}
